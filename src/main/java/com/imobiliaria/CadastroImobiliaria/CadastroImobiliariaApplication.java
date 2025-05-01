6f5fdee65a33572360a84ee355ae2c94370f8e38
Mateus Alencar <mateus.alencar3182@outlook.com>
2025-05-01 10:17:46 -0300

    first commit

---
+++ src/main/java/com/imobiliaria/CadastroImobiliaria/CadastroImobiliariaApplication.java
@@ -0,0 +1,13 @@
+package com.imobiliaria.CadastroImobiliaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroImobiliariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroImobiliariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imobiliaria/CadastroImobiliaria/CadastroImobiliariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imobiliaria.CadastroImobiliaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroImobiliariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
